b4a68ada9ff4088a65c72daa87d3f86cc9d30ac4
Christopho <christopho@solarus-games.org>
2018-06-16 12:11:21 +0200

    Don't create the window after all in -no-video mode
    Disable shader unit test in CI since no window can be created.

--- src/graphics/Video.cpp
+++ src/graphics/Video.cpp
@@ -230,7 +230,26 @@ void initialize(const Arguments& args) {
     }
   }
 
-  create_window();
+  if (context.disable_window) {
+    // Create a pixel format anyway to make surface and color operations work,
+    // even though nothing will ever be rendered.
+    context.pixel_format = SDL_AllocFormat(SDL_PIXELFORMAT_ABGR8888);
+    context.software_surface = SDL_CreateRGBSurface(
+        0,
+        320,
+        240,
+        32,
+        context.pixel_format->Rmask,
+        context.pixel_format->Gmask,
+        context.pixel_format->Bmask,
+        context.pixel_format->Amask
+    );
+    context.rgba_format = context.pixel_format;
+    context.main_renderer = SDL_CreateSoftwareRenderer(context.software_surface);
+  }
+  else {
+    create_window();
+  }
 }
 
 /**
