309415cb5ee4cb223728a11c3d9a395bff6d699e
Christopho <christopho@solarus-games.org>
2018-06-27 12:37:40 +0200

    Fix scale2x shader unit test not working on MacOS X
    Closes #1220.

--- tests/testing_quest/data/shaders/scale2x.frag.glsl
+++ tests/testing_quest/data/shaders/scale2x.frag.glsl
@@ -1,21 +1,36 @@
-#version 130
+#if __VERSION__ >= 130
+#define COMPAT_VARYING in
+#define COMPAT_TEXTURE texture
+out vec4 FragColor;
+#else
+#define COMPAT_VARYING varying
+#define FragColor gl_FragColor
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+precision mediump float;
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
 
 uniform sampler2D sol_texture;
 uniform vec2 sol_input_size;
 uniform vec2 sol_output_size;
 vec2 sol_texture_size = sol_input_size;
 
-varying vec2 tex_coords[5];
+COMPAT_VARYING vec2 tex_coords[5];
 
 void main() {
     vec4 colD, colF, colB, colH, col, tmp;
     vec2 sel;
 
-    col  = texture2D(sol_texture, tex_coords[0]);  //central (can be E0-E3)
-    colD = texture2D(sol_texture, tex_coords[1]);  //D (left)
-    colF = texture2D(sol_texture, tex_coords[2]);  //F (right)
-    colB = texture2D(sol_texture, tex_coords[3]);  //B (top)
-    colH = texture2D(sol_texture, tex_coords[4]);  //H (bottom)
+    col  = COMPAT_TEXTURE(sol_texture, tex_coords[0]);  //central (can be E0-E3)
+    colD = COMPAT_TEXTURE(sol_texture, tex_coords[1]);  //D (left)
+    colF = COMPAT_TEXTURE(sol_texture, tex_coords[2]);  //F (right)
+    colB = COMPAT_TEXTURE(sol_texture, tex_coords[3]);  //B (top)
+    colH = COMPAT_TEXTURE(sol_texture, tex_coords[4]);  //H (bottom)
 
     sel = fract(tex_coords[0] * sol_texture_size.xy);       //where are we (E0-E3)?
     //E0 is default
@@ -26,5 +41,5 @@ void main() {
         col = colD;
     }
 
-    gl_FragColor = col;
+    FragColor = col;
 }
