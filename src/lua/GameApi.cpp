aaca9f2196d1256d29e2ae83224b33995c8807aa
Christopho <christopho@solarus-games.org>
2018-09-05 12:29:29 +0200

    Allow shaders to be indexed like tables
    Closes #1267.

--- src/lua/GameApi.cpp
+++ src/lua/GameApi.cpp
@@ -110,7 +110,7 @@ void LuaContext::register_game_module() {
   const std::vector<luaL_Reg> metamethods = {
       { "__gc", userdata_meta_gc },
       { "__newindex", userdata_meta_newindex_as_table },
-      { "__index", userdata_meta_index_as_table }
+      { "__index", userdata_meta_index_as_table },
   };
 
   register_type(game_module_name, functions, methods, metamethods);
