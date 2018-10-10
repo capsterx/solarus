5db20076f6ab7c81b2c401a79f2a5d812c573523
Christopho <christopho@solarus-games.org>
2018-10-10 23:17:23 +0200

    Trailing whitespaces

--- tests/testing_quest/data/maps/custom_state/end_map.lua
+++ tests/testing_quest/data/maps/custom_state/end_map.lua
@@ -5,7 +5,7 @@ function map:on_opening_transition_finished()
   -- test if current hero state events have been called
 
   -- TODO not working yet because the engine goes to free state automatically
-  -- add state:get/set_persist_accross_maps?  
+  -- add state:get/set_persist_accross_maps?
   -- assert_equal(hero:get_state(), "custom")
   -- hero:get_state_object():collect_events()
   sol.main.exit()
