bd895eb636329bb56549060117e0eaba41fa7180
Christopho <christopho@solarus-games.org>
2019-05-31 12:07:47 +0200

    Uncomment code

--- src/core/MainLoop.cpp
+++ src/core/MainLoop.cpp
@@ -500,7 +500,7 @@ void MainLoop::check_input() {
     notify_input(*event);
     event = InputEvent::get_event();
   }
-/*
+
   // Check Lua requests.
   if (!lua_commands.empty()) {
     std::lock_guard<std::mutex> lock(lua_commands_mutex);
@@ -519,7 +519,7 @@ void MainLoop::check_input() {
       ++num_lua_commands_done;
     }
     lua_commands.clear();
-  }*/
+  }
 }
 
 void MainLoop::setup_game_icon() {
