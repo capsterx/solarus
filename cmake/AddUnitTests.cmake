75b184746e3338b1d6393b596b3092a28ae6962c
Hugo Hromic <hhromic@gmail.com>
2019-06-03 22:44:02 +0000

    Refactor CMake files (tests)
    * Split `CMakeLists.txt` into separate include files
    * Use `target_sources()` command with appropriate public/private declarations
    * The `solarus-testing` target only depends on `solarus` (dep transitivity)
    * The test targets only depend on `solarus-testing` (dep transitivity)

--- cmake/AddUnitTests.cmake
+++ cmake/AddUnitTests.cmake
@@ -4,7 +4,7 @@ add_subdirectory(tests)
 
 # Set up the root path on OSX testing target.
 if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-  set_target_properties(solarus_testing PROPERTIES
+  set_target_properties(solarus-testing PROPERTIES
     MACOSX_RPATH                       ON
     BUILD_WITH_INSTALL_RPATH           1
     INSTALL_NAME_DIR                   "@rpath"
---
+++ tests/cmake/AddTests.cmake
@@ -0,0 +1,51 @@
+# Source files of the 'src/tests' directory that are a test with a main() function
+list(APPEND TESTS_MAIN_FILES
+  src/tests/Initialization.cpp
+  src/tests/MapData.cpp
+  src/tests/LanguageData.cpp
+  src/tests/PathFinding.cpp
+  src/tests/PathMovement.cpp
+  src/tests/PixelMovement.cpp
+  src/tests/Quadtree.cpp
+  src/tests/SpriteData.cpp
+  src/tests/TilesetData.cpp
+  src/tests/ShaderData.cpp
+  src/tests/RunLuaTest.cpp
+)
+
+# Generate test executables
+foreach(TEST_MAIN_FILE ${TESTS_MAIN_FILES})
+  get_filename_component(TEST_BIN_FILE ${TEST_MAIN_FILE} NAME_WE)
+  add_executable(${TEST_BIN_FILE} "")
+  target_sources(${TEST_BIN_FILE}
+    PRIVATE
+      "${CMAKE_CURRENT_SOURCE_DIR}/${TEST_MAIN_FILE}"
+  )
+  target_link_libraries(${TEST_BIN_FILE}
+    PUBLIC
+      solarus-testing
+  )
+  set_target_properties(${TEST_BIN_FILE} PROPERTIES
+    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/bin"
+  )
+
+  # Lua test runner: add an individual test for each map.
+  if (${TEST_MAIN_FILE} MATCHES "src/tests/RunLuaTest.cpp")
+    foreach(MAP_ID ${LUA_TEST_MAPS})
+      add_test("lua/${MAP_ID}" "bin/${TEST_BIN_FILE}" -no-audio -no-video -turbo=yes "-map=${MAP_ID}" "${CMAKE_CURRENT_SOURCE_DIR}/testing_quest")
+    endforeach()
+    # Tests that want to survive on errors.
+    foreach(MAP_ID ${LUA_TEST_MAPS_NON_FATAL})
+      add_test("lua/${MAP_ID}" "bin/${TEST_BIN_FILE}" -no-audio -no-video -turbo=yes -fatal-errors=no "-map=${MAP_ID}" "${CMAKE_CURRENT_SOURCE_DIR}/testing_quest")
+    endforeach()
+    # Tests that need a window to run.
+    foreach(MAP_ID ${LUA_TEST_MAPS_REQUIRE_WINDOW})
+      add_test("lua/${MAP_ID}" "bin/${TEST_BIN_FILE}" -no-audio -turbo=yes "-map=${MAP_ID}" "${CMAKE_CURRENT_SOURCE_DIR}/testing_quest")
+    endforeach()
+  else()
+    # Normal C++ test.
+    get_filename_component(TEST_NAME "${TEST_MAIN_FILE}" NAME_WE)
+    string(TOLOWER "${TEST_NAME}" TEST_NAME)
+    add_test("${TEST_NAME}" "bin/${TEST_BIN_FILE}" -no-audio -no-video -turbo=yes "${CMAKE_CURRENT_SOURCE_DIR}/testing_quest")
+  endif()
+endforeach()
