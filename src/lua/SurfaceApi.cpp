4feff7b3041144dfd84c7107f7127aff47db64ed
Cluedrew <cluedrew.of.ink@gmail.com>
2019-06-30 17:19:42 -0400

    Fixes issue #1394. Makes the remaining userdata types indexable.

--- src/lua/SurfaceApi.cpp
+++ src/lua/SurfaceApi.cpp
@@ -80,7 +80,9 @@ void LuaContext::register_surface_module() {
   }
 
   const std::vector<luaL_Reg> metamethods = {
-      { "__gc", drawable_meta_gc }
+      { "__gc", drawable_meta_gc },
+      { "__newindex", userdata_meta_newindex_as_table },
+      { "__index", userdata_meta_index_as_table }
   };
 
   register_type(surface_module_name, functions, methods, metamethods);
