435b693f89e789b730f2b4484ad111b422c161d3
Olivier Cléro <oclero@hotmail.com>
2019-10-01 23:38:32 +0200

    Improve Solarus Launcher icon

--- gui/cmake/AddInstallTargets.cmake
+++ gui/cmake/AddInstallTargets.cmake
@@ -19,38 +19,43 @@ install(FILES
 
 # FreeDesktop compatible icons
 if(UNIX AND NOT APPLE)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/../cmake/icons/solarus_icon_16.png
-    DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/16x16/apps RENAME solarus.png)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/../cmake/icons/solarus_icon_24.png
-    DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/24x24/apps RENAME solarus.png)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/../cmake/icons/solarus_icon_32.png
-    DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/32x32/apps RENAME solarus.png)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/../cmake/icons/solarus_icon_48.png
-    DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/48x48/apps RENAME solarus.png)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/../cmake/icons/solarus_icon_64.png
-    DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/64x64/apps RENAME solarus.png)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/../cmake/icons/solarus_icon_128.png
-    DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/128x128/apps RENAME solarus.png)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/../cmake/icons/solarus_icon_256.png
-    DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/256x256/apps RENAME solarus.png)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/../cmake/icons/solarus_icon_512.png
-    DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/512x512/apps RENAME solarus.png)
+  # Pixmap icons for sizes under 48x48 pixels
+  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_16.png
+    DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/16x16/apps RENAME solarus-launcher.png)
+    install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_20.png
+    DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/20x20/apps RENAME solarus-launcher.png)
+  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_24.png
+    DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/24x24/apps RENAME solarus-launcher.png)
+  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_32.png
+    DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/32x32/apps RENAME solarus-launcher.png)
+  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_48.png
+    DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/48x48/apps RENAME solarus-launcher.png)
+  
+  # Pixmap icon for desktop that don't support multiple sizes
+  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_512.png
+    DESTINATION ${CMAKE_INSTALL_PREFIX}/share/pixmaps RENAME solarus-launcher.png)
+
+  # Vector icons, automatically chosen for sizes above 48x48 pixels
+  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon.svg
+    DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/scalable/apps RENAME solarus-launcher.svg)
+  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_symbolic.svg
+    DESTINATION ${CMAKE_INSTALL_PREFIX}/share/icons/hicolor/symbolic/apps RENAME solarus-launcher-symbolic.svg)
 endif(UNIX AND NOT APPLE)
 
 # FreeDesktop compatible start menu launcher
 if(UNIX AND NOT APPLE)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/../gui/resources/solarus.desktop
+  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/solarus-launcher.desktop
     DESTINATION ${CMAKE_INSTALL_PREFIX}/share/applications)
 endif(UNIX AND NOT APPLE)
 
 # AppStream compatible software gallery metadata
 if(UNIX AND NOT APPLE)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/../gui/resources/solarus.appdata.xml
+  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/solarus-launcher.appdata.xml
     DESTINATION ${CMAKE_INSTALL_PREFIX}/share/appdata)
 endif(UNIX AND NOT APPLE)
 
 # Linux Manpage
 if(UNIX AND NOT APPLE)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/../solarus-launcher.6
+  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/solarus-launcher.6
     DESTINATION ${SOLARUS_MANUAL_INSTALL_DESTINATION}/man6)
 endif(UNIX AND NOT APPLE)
