330b63383daa4ce31a6262054f860200c8487e97
Christopho <christopho@solarus-games.org>
2020-04-03 10:26:42 +0200

    Unit test for state:set_affected_by_ground("hole", false)

--- tests/testing_quest/data/maps/bugs/1416_state_set_affected_by_ground_speed.lua
+++ tests/testing_quest/data/maps/bugs/1416_state_set_affected_by_ground_speed.lua
@@ -5,20 +5,16 @@ function map:on_opening_transition_finished()
 
   local state = sol.state.create()
   state:set_affected_by_ground("shallow_water", false)
+  state:set_affected_by_ground("hole", false)
   hero:start_state(state)
-  
+
   assert_equal(hero:get_ground_below(), "shallow_water")
   game:simulate_command_pressed("right")
   
-  sol.timer.start(map, 10, function()
+  sol.timer.start(map, 1000, function()
+    assert(hero:get_movement() ~= nil)
     assert_equal(hero:get_movement():get_speed(), 88)  -- The ground should not affect the speed.
+    assert(hero:get_sprite("ground") == nil)
     sol.main.exit()
-  end)
-
-  sol.timer.start(map, 500, function()
-    if hero:get_movement() ~= nil then
-      print(hero:get_movement():get_speed())
-    end
-    return true
   end)
 end
